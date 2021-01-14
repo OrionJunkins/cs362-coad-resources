eb84ba0890bf8e8b4c4b110f8868d5519094285a
Orion Junkins <junkinso@oregonstate.edu>
2021-01-14 11:32:47 -0800

    Region instantiation tests started

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
+  #let (region.new)
+  it 'exists' do
+    Region.new
+  end
 
+  it 'has a name' do
+    region = Region.new
+    expect(region).to respond_to(:name)
+  end
 end
