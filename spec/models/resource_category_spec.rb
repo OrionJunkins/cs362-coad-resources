ce0712ec56f1b9c5e4f2461b3b2cb7f553a334f7
Orion Junkins <junkinso@oregonstate.edu>
2021-01-18 20:13:14 -0800

    Resource_category characterization spec

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
-  it 'exists' do
-    resource_category = ResourceCategory.new 
+  let (:resource_category) do
+    ResourceCategory.new 
   end
+
+  it 'has a name' do
+    expect(resource_category).to respond_to(:name)
+  end
+
 end
