aa39e31379904967e17e20dde386a1d6ecc93200
Orion Junkins <junkinso@oregonstate.edu>
2021-01-18 20:23:58 -0800

    ticket characterization specs

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -5,8 +5,20 @@ RSpec.describe Ticket, type: :model do
     Ticket.new
   end
 
-  it 'exists' do 
-    ticket
+  it 'has a name' do 
+    expect(ticket).to respond_to(:name)
+  end
+
+  it 'has a phone' do 
+    expect(ticket).to respond_to(:phone)
+  end
+
+  it 'has a region_id' do 
+    expect(ticket).to respond_to(:region_id)
+  end
+
+  it 'has a resource_category_id' do 
+    expect(ticket).to respond_to(:resource_category_id)
   end
 
 end
