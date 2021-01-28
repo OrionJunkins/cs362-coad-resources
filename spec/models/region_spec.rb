a14bb91ea5a5ccac4b93fdeedf3a6abe4f58e4f3
Orion Junkins <junkinso@oregonstate.edu>
2021-01-27 19:22:34 -0800

    case insensitivity added to fix name uniqueness issue

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe Region, type: :model do
     it { should validate_presence_of(:name) } 
     it { should validate_length_of(:name).is_at_least(1) }
     it { should validate_length_of(:name).is_at_most(255) }
-    #it { should validate_uniqueness_of(:name).scoped_to(:organization) } #Fails
+    it { should validate_uniqueness_of(:name).case_insensitive } 
   end
 
   describe 'associations:' do
--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,16 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+
   let (:resource_category) do
     ResourceCategory.new 
   end
 
-  it 'has a name' do
-    expect(resource_category).to respond_to(:name)
+
+  describe 'attributes:' do
+
+    it 'has a name' do
+      expect(resource_category).to respond_to(:name)
+    end
+  
+    it 'has an active status' do
+      expect(resource_category).to respond_to(:active)
+    end
   end
 
-  it 'has an active status' do
-    expect(resource_category).to respond_to(:active)
+
+  describe 'validation behavior:' do
+    it { should validate_presence_of(:name) } 
+    it { should validate_length_of(:name)
+    .is_at_least(1)
+    .is_at_most(255)
+    .on(:create) }
+    it { should validate_uniqueness_of(:name).case_insensitive } 
   end
 
+  describe 'associations:' do
+
+    it 'has many tickets' do
+      should have_many(:tickets)
+    end
+
+  end
+
+  describe 'behavior:' do 
+
+    it 'has a string representation of its name' do
+      name = "Fake name"
+      resource_category.name = name
+      str_rep = resource_category.to_s
+      expect(str_rep).to eq(name)
+    end
+
+    it 'unspecified resource category exists upon search' do 
+      expect(ResourceCategory.unspecified.to_s). to eq('Unspecified')
+    end
+
+  end
+
+
+  
 end
