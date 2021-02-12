ffd25fa0046fd7f1cefdf316624ffd9f68f7e661
Orion Junkins <junkinso@oregonstate.edu>
2021-02-12 12:31:25 -0800

    RC controller spec complete

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -1,5 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
+  context 'unauthenticated users' do
+    it 'redirects to the sign_in screen' do
+      get :index          
+      expect(response).to redirect_to(new_user_session_path)
 
+      get :show, params: {id: 'fake'}
+      expect(response).to redirect_to(new_user_session_path)
+
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+
+      post :create
+      expect(response).to redirect_to(new_user_session_path)
+
+      get :edit, params: {id: 'fake'}
+      expect(response).to redirect_to(new_user_session_path)
+
+      patch :update, params: {id: 'fake'}
+      expect(response).to redirect_to(new_user_session_path)
+
+      patch :activate, params: {id: 'fake'}
+      expect(response).to redirect_to(new_user_session_path)
+
+      patch :deactivate, params: {id: 'fake'}
+      expect(response).to redirect_to(new_user_session_path)
+
+      delete :destroy, params: {id: 'fake'}
+      expect(response).to redirect_to(new_user_session_path)
+    end
+
+  end
+
+
+
+  context 'organization users' do
+    before do
+      organization_user = build(:user, :organization)
+      allow(request.env['warden']).to receive(:authenticate!).and_return(organization_user)
+      allow(controller).to receive(:current_user).and_return(organization_user) # Override current_user()
+    end
+
+    it 'redirects to the dashboard url' do
+      get :index
+      expect(response).to redirect_to(dashboard_url)
+
+      get :show, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+
+      get :new
+      expect(response).to redirect_to(dashboard_url)
+
+      post :create
+      expect(response).to redirect_to(dashboard_url)
+
+      get :edit, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+
+      patch :update, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+
+      patch :activate, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+
+      patch :deactivate, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+
+      delete :destroy, params: {id: 'fake'}
+      expect(response).to redirect_to(dashboard_url)
+
+    end
+
+  end
 end
