bc27fea1ed1c34564398c837d534704ee1007192
Orion Junkins <junkinso@oregonstate.edu>
2021-03-09 11:36:56 -0800

    Application helper completed with full_title tests

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe 'full_title' do
-    it 'gives base title if called with no param' do end
-    it 'gives a combined title if called with a normal page title' do end
+  describe '#full_title' do
+    it 'gives base title if called with no param' do 
+      full_page_title = full_title()
+      expect(full_page_title).to eq('Disaster Resource Network')
+    end
+    
+    it 'gives a combined title if called with a normal page title' do 
+      page_title = 'Fake Page'
+      expected_full_title = 'Fake Page | Disaster Resource Network'
+      full_page_title = full_title(page_title)
+      expect(full_page_title).to eq(expected_full_title)
+    end
   end
 end
