5c308163435ad7b3e592b5f870894f2f4216bfc5
Orion Junkins <junkinso@oregonstate.edu>
2021-03-09 12:01:23 -0800

    HTML checking added to admin delete actions

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -4,12 +4,13 @@ RSpec.describe 'Deleting a Region', type: :feature do
 
   let(:admin) {create(:user, :admin)}
 
-  specify 'shows a list of region names' do
+  specify 'renders a notification after a region is deleted' do
     region = create(:region)
     log_in_as(admin)
     click_on 'Regions'
     click_on region.name
     click_on 'Delete'
     expect(page.body).to have_text('Region ' + region.name + ' was deleted.')
+    expect(page.body).to_not have_link(region.name)
   end
 end
--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
 
+  let(:admin) {create(:user, :admin)}
+
+  specify 'renders a notification after a resource category is deleted' do
+    resource_category = create(:resource_category)
+    log_in_as(admin)
+    click_on 'Categories'
+    click_on resource_category.name
+    click_on 'Delete'
+    expect(page.body).to have_text('Category ' + resource_category.name + ' was deleted.')
+    expect(page.body).to_not have_link(resource_category.name)
+    
+  end
 end
