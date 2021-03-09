5e37f5f4f4a79a500492ab385ee4d3aa25c0db84
Orion Junkins <junkinso@oregonstate.edu>
2021-03-09 11:33:23 -0800

    Dashboard helper completed with doubles

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -12,7 +12,7 @@ require 'rails_helper'
 # end
 RSpec.describe DashboardHelper, type: :helper do
   describe '#dashboard_for' do
-    context 'admin user'
+    context 'admin user' do
       it 'returns admin_dashboard' do
         user = double()
         allow(user).to receive(:admin?).and_return(true)
@@ -23,8 +23,24 @@ RSpec.describe DashboardHelper, type: :helper do
     context 'organization users with a submitted organization' do
       it 'returns organization_submitted_dashboard' do
         user = double()
+        organization = double()
         allow(user).to receive(:admin?) {false}
-        allow(user).to recieve(:organization, :submitted) {true}
+        allow(user).to receive(:organization) {organization}
+        allow(organization).to receive(:submitted?) {true}
+
+        expect(dashboard_for(user)).to eq('organization_submitted_dashboard')
+      end
+    end
+
+    context 'organization users with an unsubmitted, approved organization' do
+      it 'returns organization_submitted_dashboard' do
+        user = double()
+        organization = double()
+        allow(user).to receive(:admin?) {false}
+        allow(user).to receive(:organization) {organization}
+        allow(organization).to receive(:submitted?) {false}
+        allow(organization).to receive(:approved?) {true}
+        expect(dashboard_for(user)).to eq('organization_approved_dashboard')
       end
     end
     
