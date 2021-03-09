24dbed95cab41839cc24fade2825d615e16581bb
Orion Junkins <junkinso@oregonstate.edu>
2021-03-09 12:04:32 -0800

    Admin ticket delete test added with HTML checking

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
+  let(:admin) {create(:user, :admin)}
 
+  specify 'renders a notification after a ticket is deleted and no longer lists the ticket' do
+    ticket = create(:ticket)
+    log_in_as(admin)
+    click_on 'Dashboard'
+    click_on ticket.name
+    click_on 'Delete'
+    expect(page.body).to have_text('Ticket ' + ticket.id.to_s + ' was deleted.')
+    expect(page.body).to_not have_link(ticket.name)
+  end
 end
