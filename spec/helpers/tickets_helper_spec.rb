637fa48b99e46159111423642af3eba79cbfd515
Orion Junkins <junkinso@oregonstate.edu>
2021-03-09 11:40:49 -0800

    format_phone_number tests included

--- spec/helpers/tickets_helper_spec.rb
+++ spec/helpers/tickets_helper_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the TIcketsHelper. For example:
-#
-# describe TIcketsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe TicketsHelper, type: :helper do
+  describe '#format_phone_number' do
+    it 'formats a valid, but poorly formatted phone number correctly' do
+      input_phone_number = '54 1-555, 1---23/4_'
+      expected_formatted_phone_number = '+15415551234'
+      formatted_phone_number = format_phone_number(input_phone_number)
+      expect(formatted_phone_number).to eq(expected_formatted_phone_number)
+    end
 
+    it 'gives nil if input is invalid' do
+      input_phone_number = 'test phrase'
+      expected_formatted_phone_number = nil
+      formatted_phone_number = format_phone_number(input_phone_number)
+      expect(formatted_phone_number).to eq(expected_formatted_phone_number)
+    end
+
+  end
 end
