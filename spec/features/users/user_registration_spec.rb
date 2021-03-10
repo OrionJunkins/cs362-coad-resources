e6847af6b73e3a2a23fee5593db59a573493b9b8
Orion Junkins <junkinso@oregonstate.edu>
2021-03-10 14:12:52 -0800

    Started registration spec, but expectation still fails

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
+  specify('user with valid information can register') do
+    user_email = 'test@domain.com'
+    password = 'password'
+    visit root_path
+    click_on 'Sign up'
+    fill_in 'Email address', with: user_email
+    fill_in 'Password', with: password
+    fill_in 'Password confirmation', with: password
+    within "form" do
+      click_on 'Sign up'
+    end
+    
+    #expect(page.body).to have_text('account created')
+  end
 
 end
