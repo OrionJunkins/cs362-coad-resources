2d0dda1b7cc865043df0ba9ed507701d28c3407f
Orion Junkins <junkinso@oregonstate.edu>
2021-03-10 14:12:34 -0800

    capture spec started, but incomplete

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
+  let(:user) {create(:user, :organization)}
 
+  specify 'a captured ticket appears in the list of "my captured" status tickets' do
+    ticket = create(:ticket)
+    log_in_as(user)
+    click_on 'Dashboard'
+    click_on 'Tickets'
+    click_on ticket.name
+    click on 'Capture'
+    click_on 'Tickets'
+    fill_in 'Status', with: 'My captured'
+    click_on 'Tickets'
+  end
 end
