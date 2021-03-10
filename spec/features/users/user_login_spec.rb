ffc1c3ce2c974a62203ee215f077ddcf0f5a7a38
Orion Junkins <junkinso@oregonstate.edu>
2021-03-09 18:16:07 -0800

    basic log in tests added

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
+  specify 'logs in a user with valid credentials' do
+    user = create(:user)
+    visit root_path
+    click_on 'Log in'
+    fill_in 'Email address', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Sign in'
+    expect(page.body).to have_text('Signed in')
+  end
 
+  specify 'rejects a user with invalid credentials' do
+    user = create(:user)
+    invalid_password = "Invalid"
+    visit root_path
+    click_on 'Log in'
+    fill_in 'Email address', with: user.email
+    fill_in 'Password', with: invalid_password
+    click_on 'Sign in'
+    expect(page.body).to have_text('Invalid Email or password.')
+  end
+  
 end
