cc17c0217f112411d78d0ba5d4321dd05cdc2d33
Orion Junkins <junkinso@oregonstate.edu>
2021-03-14 15:45:00 -0700

    Organization approval under test

--- spec/features/organizations/approve_organization_spec.rb
+++ spec/features/organizations/approve_organization_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Approving an organization', type: :feature do
+  
+  let(:admin) {create(:user, :admin)}
+  let(:organization) {create(:organization, :unapproved)}
+  
 
+  specify 'pending organization can be approved' do
+    log_in_as(admin)
+    
+    expect(organization.status).to eq("submitted")
+    click_on 'Organizations'
+    click_on 'Pending'
+    click_on organization.name
+    click_on 'Approve'
+    expect(page.body).to have_text('been approved')
+  end
 end
