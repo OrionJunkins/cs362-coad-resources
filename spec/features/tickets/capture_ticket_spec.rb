03e1fe637558582ab7bdb7d2f25139dcc1a59183
Orion Junkins <junkinso@oregonstate.edu>
2021-03-14 15:45:33 -0700

    Ticket capturing spec fixed and simplified

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,21 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
-  let(:user) {create(:user, :organization)}
+  let(:user) {create(:user, :with_organization)}
 
   specify 'users can capture a ticket' do
-    skip
-    user = create(:user, :organization)
-    user.organization = create(:organization)
+    log_in_as(user)
     ticket = create(:ticket, :open)
     visit login_path
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    find_by_id('commit').click
     visit ticket_path(ticket)
     click_on 'Capture'
     visit ticket_path(ticket)
     expect(page.body).to have_text('Release')
-    expect(page.body).to have_text(:user.organization.name)
   end
 end
